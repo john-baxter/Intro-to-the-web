26bad32d93021a077af2bf44e7fbe689b1db1987
john-baxter <john.g.baxter@googlemail.com>
2020-03-02 17:01:43 +0000

    first commit - just got to the bit with the cat.

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+  "Hello World"
+end
+
+get '/secret' do
+  "message of my choosing"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src=http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png>
+  </div>"
+end
